3df4bc372ccb221f1c25360fec753f11e263af69
hanseunghyeon1 <113760476+hanseunghyeon1@users.noreply.github.com>
2023-03-21 14:23:00 +0900

    [Bronze I] Title: 세로읽기, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str;
+		String[][] ans = new String[5][15];
+		for(int i=0; i<5; i++) {
+			str = br.readLine();
+			for(int j=0; j<str.length(); j++) {
+				ans[i][j] = String.valueOf(str.charAt(j));
+			}
+		}
+		for(int i=0; i<15; i++) {
+			for(int j=0; j<5; j++) {
+				if(ans[j][i]==null) continue;
+				bw.write(ans[j][i]+"");
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+}
