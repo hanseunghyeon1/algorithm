d7e4271864d491c8108da8b477a5f6924a66e283
hanseunghyeon1 <113760476+hanseunghyeon1@users.noreply.github.com>
2023-03-22 13:47:23 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		
+		if(a==b&&b==c&&c==60) {
+			bw.write("Equilateral\n");
+		}else if(a+b+c==180&&(a==b||b==c||a==c)) {
+			bw.write("Isosceles\n");
+		}else if(a+b+c==180&&(a!=b||b!=c||a!=c)) {
+			bw.write("Scalene\n");
+		}else {
+			bw.write("Error\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
