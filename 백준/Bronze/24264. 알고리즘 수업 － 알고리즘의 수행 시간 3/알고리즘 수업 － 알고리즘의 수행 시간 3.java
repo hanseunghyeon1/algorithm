80f6548bd3db02427d214b848ae3f16ca11f86e2
hanseunghyeon1 <113760476+hanseunghyeon1@users.noreply.github.com>
2023-03-22 16:45:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 200 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		System.out.println(a*a);
+		System.out.println(2);
+		sc.close();
+	}
+}
