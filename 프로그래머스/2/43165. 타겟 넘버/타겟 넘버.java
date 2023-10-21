12dc54fc90195b97e6c34e16e94b84ef84b46902
Loopy <113760476+hanseunghyeon1@users.noreply.github.com>
2023-10-21 11:34:07 +0900

    [level 2] Title: 타겟 넘버, Time: 0.40 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,15 +2,15 @@
 class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, 0, target, 0);
-    }
-    public int dfs(int[] numbers, int depth, int target, int sum){
-        if(depth == numbers.length){
-            if(target == sum) answer++;
-        } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]);
-            dfs(numbers, depth + 1, target, sum - numbers[depth]);
-        }
+        dfs(numbers, 0, target, 0);
         return answer;
+    }
+    public void dfs(int[] numbers, int depth, int target, int sum){
+        if(depth == numbers.length){
+            if(sum == target) answer++;
+        }else{
+            dfs(numbers, depth+1, target, sum + numbers[depth]);
+            dfs(numbers, depth+1, target, sum - numbers[depth]);
+        }
     }
 }
