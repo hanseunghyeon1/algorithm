47c3ca5045944775123a0afd86c461cfdbc96ac7
Loopy <113760476+hanseunghyeon1@users.noreply.github.com>
2024-05-17 15:58:44 +0900

    [Silver II] Title: 스택 수열, Time: 4232 ms, Memory: 37516 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+numList = list(int(input()) for i in range(n))
+
+deq = deque()
+num = 1
+result = ''
+for i in range(n):
+    a = numList[i]
+    if a >= num:
+        while a >= num:
+            deq.append(num)
+            num += 1
+            result += '+\n'
+        deq.pop()
+        result += '-\n'
+    else:
+        b = deq.pop()
+        if b > a:
+            print("NO")
+            result = False
+            break
+        else:
+            result += '-\n'
+if result:
+    print(result)
